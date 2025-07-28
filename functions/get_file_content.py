e0044b6e807ee9007db4a36db95e5f01d3944ed1
keegs37 <keegs37@github.com>
2025-07-28 13:33:02 -0700

    added get_file_info function

---
+++ functions/get_file_content.py
@@ -0,0 +1,25 @@
+import os
+from functions.config import MAX_CHARACTERS
+def get_file_content(working_directory, file_path):
+    # File to list the contents of
+    requested_file = os.path.join(working_directory, file_path)
+    
+    try:
+        # Prevent directory traversal outside the working directory
+        if os.path.abspath(working_directory) not in os.path.abspath(requested_file):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        # Verify the requested file is an actual file
+        if os.path.isfile(requested_file) == False:
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+        # Open file, and return contents truncated to MAX_CHARACTERS value (10,000) if applicable
+        with open(requested_file, 'r') as file:
+            content = file.read()
+            if len(content) > MAX_CHARACTERS:
+                return f"{content[:MAX_CHARACTERS]} [...File {os.path.abspath(requested_file)} truncated at 10000 characters]"
+
+            return content
+
+    # Return error message if any standard library call fails
+    except Exception as e:
+        return f"Error: {e}"
