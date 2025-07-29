46421a562d8842650543311b64d06492b417f552
keegs37 <keegs37@github.com>
2025-07-29 10:41:52 -0700

    updated run_python function and added a system prompt to the agent

--- functions/run_python.py
+++ functions/run_python.py
@@ -2,20 +2,39 @@ import os
 import subprocess
 def run_python_file(working_directory, file_path, args=[]):
     requested_file = os.path.join(os.path.abspath(working_directory), file_path)
+    try:
 
-    # Prevent directory traversal outside of working directory
-    if os.path.abspath(working_directory) not in os.path.abspath(requested_file):
-        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
-    
-    if os.path.isfile(os.path.abspath(requested_file)) == False:
-        return f'Error: File "{file_path}" not found.'
+        # Prevent directory traversal outside of working directory
+        if os.path.abspath(working_directory) not in os.path.abspath(requested_file):
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        # Check to see if the file exists
+        if os.path.isfile(os.path.abspath(requested_file)) == False:
+            return f'Error: File "{file_path}" not found.'
 
-    
-    if file_path[len(file_path) - 3:len(file_path)] != ".py":
-        return f"Error: '{file_path}' is not a Python file."
-    
-    # Setting commands for subprocess.run including the file to run
-    command = ["python", os.path.abspath(requested_file)] + args
-    subprocess.run(command, stdin = True, stdout = True, timeout = 30, cwd = os.path.abspath(working_directory))
+        # Make sure the file is a .py file 
+        if file_path[len(file_path) - 3:len(file_path)] != ".py":
+            return f"Error: '{file_path}' is not a Python file."
+        
+        # Setting commands for subprocess.run including the file to run
+        command = ["python", os.path.abspath(requested_file)] + args
+        # Run the requested file with a timeout of 30 seconds, capture std in and out and run in the working_directory
+        process = subprocess.run(command, capture_output = True, text = True, timeout = 30, cwd = os.path.abspath(working_directory))
+        
+        # List to store return values
+        return_string = []
 
-print(run_python_file("calculator", "main.py",["3 + 2"]))
+        # Add stdout and stderr to the return string 
+        return_string.append(f"STDOUT: {process.stdout}")
+        return_string.append(f"STDERR: {process.stderr}")
+
+        # Check if there were errors or no output and return values accordingly
+        if process.returncode > 0:
+            return_string.append(f"Process exited with code {process.returncode}")
+        if not process.stdout:
+            return_string.append("No output produced") 
+        return "\n".join(return_string)
+
+    # Return error message if any standard library call fails
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+
--- main.py
+++ main.py
@@ -3,25 +3,44 @@ import sys
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+
+system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+# Load api key and initialize the ai client
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+# Check if a prompt was provided, error if not
 if len(sys.argv) < 2:
     print('Error: No prompt provided. Usage: python3 main.py "Your prompt here"')
     sys.exit(1)
 
+# Pass the user prompt from cli to make a Content instance 
 user_prompt = sys.argv[1]
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
+
+# Pass user prompt to the AI model and capture the response
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=messages
+    model='gemini-2.0-flash-001', contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt)
 )
-print(response.text)
+# List for return string 
+return_string = []
+
+# Add AI response to return_string
+return_string.append(response.text)
+
+# If there are no "--" flags return the AI respond only
 if len(sys.argv) < 3:
+    print("\n".join(return_string))
     sys.exit()
+
+# If --verbose flag is used return more info
 if "--verbose" in sys.argv[2]:
-    print(f"User prompt: {sys.argv[1]}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    return_string.append(f"User prompt: {sys.argv[1]}")
+    return_string.append(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    return_string.append(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+print("\n".join(return_string))
